5c386f7043688cbec564a815f1e24cadd1d5c5c1
hjy <hjy666666@sjtu.edu.cn>
2024-12-29 15:08:21 +0800

    complete store.hpp

--- store.hpp
+++ store.hpp
@@ -8,9 +8,8 @@ using std::string;
 using std::fstream;
 using std::ifstream;
 using std::ofstream;
-
+template<class T>
 class nodeHead;
-const int sizeof_nodeHead = 160;
 template<class T, int info_len = 2>
 class MemoryRiver {
 private:
@@ -26,10 +25,12 @@ public:
     ~MemoryRiver() {
         file.close();
     }
-
+    void open() {
+        file.open(file_name);
+    }
     void initialise(string FN = "") {
         if (FN != "") file_name = FN;
-        file.open(file_name, std::ios::out);
+        file.open(file_name, std::ios::in);
         int tmp = 0;
         for (int i = 0; i < info_len; ++i)
             file.write(reinterpret_cast<char *>(&tmp), sizeof(int));
@@ -78,318 +79,207 @@ public:
         file.read(reinterpret_cast<char*>(&t), sizeof(T));
         /* your code here */
     }
-    void Read(nodeHead *all,int block_cnt) {
+    void ReadAll(T *all,int block_cnt) {
         file.seekg(8);
-        file.read(reinterpret_cast<char *>(all), block_cnt * sizeof_nodeHead);
+        file.read(reinterpret_cast<char *>(all), block_cnt * sizeof(T));
     }
-    void Update(nodeHead *all,int block_cnt) {
+    void UpdateAll(T *all,int block_cnt) {
         file.seekp(8);
-        file.write(reinterpret_cast<char *>(all), block_cnt * sizeof_nodeHead);
+        file.write(reinterpret_cast<char *>(all), block_cnt * sizeof(T));
     }
 };
-
 const int data_cnt_limit = 300;
 const int data_max = 400;
-int block_cnt;
-struct Data {
-    Data(string index,int value) {
-        for(int i = 0;i < index.length();i++) {
-            this->index[i] = index[i];
-        }
-        this->value = value;
-    }
-    bool operator==(const Data &other) const {
-        if(value != other.value) {
-            return false;
-        }
-        for(int i = 0;i < 65;++i) {
-            if(index[i] != other.index[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-    void initialise() {
-        for(int i = 0;i < 65;++i) {
-            index[i] = 0;
-        }
-        value = 0;
-    }
-    Data(){};
-    char index[65]{};
-    int value = 0;
+template<class T>
+class block {
+public:
+    T data[data_max]{};
+    int data_cnt = 0;
 };
+template<class T>
 class nodeHead {
 public:
-    nodeHead() {
-        for(int i = 0;i < 65;i++) {
-            data_max.index[i] = 1;
-            data_min.index[i] = 0;
-        }
-    }
-    Data data_max;
-    Data data_min;
+    nodeHead(T data_max,T data_min) :data_max(data_max),data_min(data_min){}
+    nodeHead(){};
+    T data_max{};
+    T data_min{};
     int head_pos = 0;
     int block_pos = 0;
     int head_num = 0;
     int next_num = 0;
-}all[6000]{};
-
-
-class block {
-public:
-    Data data[data_max]{};
-    int data_cnt = 0;
 };
-bool cmp_index(string &a,string &b) {
-    if(a.length() != b.length()) {
-        return a.length() > b.length();
-    }
-    for(int i = 0;i < a.length();i++) {
-        if(a[i] != b[i]) {
-            return a[i] > b[i];
+template<class T>
+class block_operator {
+public:
+    block_operator(string head_name,string body_name,T data_max,T data_min){
+        head_memory_river.file_name = head_name;
+        block_memory_river.file_name = body_name;
+        head_memory_river.open();
+        block_memory_river.open();
+        ifstream h(head_name);
+        ifstream b(body_name);
+        if(!h) {
+            head_memory_river.initialise(head_name);
+            head_memory_river.write_info(2,1);
+            nodeHead<T> begin(data_max,data_min);
+            begin.head_pos = head_memory_river.write(begin);
+            begin.head_num = 0;
+            begin.next_num = 1;
+            begin.block_pos = 8;
+            nodeHead<T> end;
+            end.head_pos = 8 + sizeof(nodeHead<T>);
+            end.head_num = 1;
+            begin.next_num = 1;
+            head_memory_river.write(end);
+            begin.next_num = end.head_num;
+            head_memory_river.update(begin,8);
         }
-    }
-    return true;
-}//大于等于返回true
-bool cmp_index(char *a,char *b) {
-    string sa = a,sb = b;
-    if(sa.length() != sb.length()) {
-        return sa.length() > sb.length();
-    }
-    for(int i = 0;i < sa.length();i++) {
-        if(a[i] != b[i]) {
-            return a[i] > b[i];
+        if(!b) {
+            block_memory_river.initialise(body_name);
+            block<T> begin;
+            block_memory_river.write(begin);
         }
+        head_memory_river.get_info(block_cnt,1);
+        head_memory_river.ReadAll(all,block_cnt);
     }
-    return true;
-}//大于等于返回true
-bool cmp_data(Data &a,Data &b) {
-    string sa = a.index,sb = b.index;
-    if(sa.length() != sb.length()) {
-        return sa.length() > sb.length();
+    ~block_operator() {
+        head_memory_river.UpdateAll(all,block_cnt);
+        head_memory_river.write_info(block_cnt,1);
     }
-    for(int i = 0;i < sa.length();i++) {
-        if(sa[i] != sb[i]) {
-            return sa[i] > sb[i];
-        }
-    }
-    if(a.value != b.value) {
-        return a.value > b.value;
-    }
-    return true;
-}//大于等于返回true
-
-void insert(MemoryRiver<block> &block_memory_river,
-  string &index,int value) {
-    int num = 0;
-    Data target(index,value);
-    while (num != 1) {
-        if(cmp_data(all[num].data_max,target) && cmp_data(target,all[num].data_min)) {
-            break;
-        }
-        num = all[num].next_num;
-    }
-    block now;
-    block_memory_river.read(now,all[num].block_pos);
-    int l = 0,r = now.data_cnt;
-    while(l < r) {
-        int mid = (l + r) / 2;
-        if(!cmp_data(now.data[mid],target)) {
-            l = mid + 1;
-        }else {
-            r = mid;
-        }
-    }
-    if(now.data[l] == target) {
-        return;
-    }
-    if(!cmp_data(now.data[now.data_cnt - 1],target)) {
-        now.data[now.data_cnt] = target;
-    }else if(cmp_data(now.data[0],target)) {
-        for(int i = now.data_cnt - 1;i >= 0;--i) {
-            now.data[i + 1] = now.data[i];
-        }
-        now.data[0] = target;
-    }else {
-        for(int i = now.data_cnt - 1;i >= l;--i) {
-            now.data[i + 1] = now.data[i];
-        }
-        now.data[l] = target;
-    }
-    now.data_cnt++;
-    if(now.data_cnt > data_cnt_limit) {
-        block_cnt++;
-        block new_block;
-        nodeHead &next_head = all[all[num].next_num];
-        nodeHead &new_head = all[block_cnt - 1];
-        for(int i = 0;i < data_cnt_limit / 2;++i) {
-            std::swap(new_block.data[data_cnt_limit / 2 - 1 - i],now.data[now.data_cnt - 1 - i]);
-        }
-        new_block.data_cnt = data_cnt_limit / 2;
-        now.data_cnt -= data_cnt_limit / 2;
-        new_head.data_max = all[num].data_max;
-        all[num].data_max = now.data[now.data_cnt - 1];
-        new_head.data_min = now.data[now.data_cnt - 1];
-        new_head.data_min.value++;
-        new_head.block_pos = block_memory_river.write(new_block);
-        new_head.head_num = block_cnt - 1;
-        new_head.head_pos = sizeof_nodeHead * new_head.head_num + 8;
-        all[num].next_num = new_head.head_num;
-        new_head.next_num = next_head.head_num;
-        block_memory_river.update(now,all[num].block_pos);
-        all[new_head.head_num] = new_head;
-    }else {
-        block_memory_river.update(now,all[num].block_pos);
-    }
-}
-
-void Delete(MemoryRiver<block> &block_memory_river,
-  string &index,int value) {
-    int num = 0;
-    Data target(index,value);
-    while (num != 1) {
-        if(cmp_data(all[num].data_max,target) && cmp_data(target,all[num].data_min)) {
-            break;
-        }
-        num = all[num].next_num;
-    }
-    block now;
-    block_memory_river.read(now,all[num].block_pos);
-    int l = 0,r = now.data_cnt;
-    while(l < r) {
-        int mid = (l + r) / 2;
-        if(!cmp_data(now.data[mid],target)) {
-            l = mid + 1;
-        }else {
-            r = mid;
-        }
-    }
-    if(now.data[l] == target) {
-        now.data[l].initialise();
-        for(int i = l;i < now.data_cnt;++i) {
-            now.data[i] = now.data[i + 1];
-        }
-        now.data_cnt--;
-        block_memory_river.update(now,all[num].block_pos);
-    }else {
-        return;
-    }
-}
-int find_cnt = 0;
-void find(MemoryRiver<block> &block_memory_river,
-  string &index) {
-    find_cnt++;
-    if(find_cnt == 114) {
-        find_cnt = 114;
-    }
-    int num = 0;
-    Data target(index,0);
-    char *Max = all[num].data_max.index,*Min = all[num].data_min.index;
-    while(num != 1) {
-        if(cmp_index(Max,&index[0]) && cmp_index(&index[0],Min)) {
-            break;
-        }
-        num = all[num].next_num;
-        Max = all[num].data_max.index;
-        Min = all[num].data_min.index;
-    }
-    int cnt = 0;
-    block now;
-    block_memory_river.read(now,all[num].block_pos);
-    while(cmp_index(Max,&index[0]) && cmp_index(&index[0],Min) && num != 1) {
-        if(now.data_cnt == 0) {
+    void insert(T &a) {
+        int num = 0;
+        while (num != 1) {
+            if(all[num].data_max >= a && a >= all[num].data_min) {
+                break;
+            }
             num = all[num].next_num;
-            block_memory_river.read(now,all[num].block_pos);
-            continue;
         }
+        block<T> now;
+        block_memory_river.read(now,all[num].block_pos);
         int l = 0,r = now.data_cnt;
         while(l < r) {
             int mid = (l + r) / 2;
-            if(!cmp_data(now.data[mid],target)) {
+            if(now.data[mid] < a) {
                 l = mid + 1;
             }else {
                 r = mid;
             }
         }
-        string l_string = now.data[l].index;
-        string t = target.index;
-        /*if(l_string != t) {
-            if(cnt == 0) {
-                std::cout << "null\n";
-                return;
-            }
-            std::cout << '\n';
+        if(now.data[l] == a) {
             return;
-        }*/
-        while(l_string == t && l < now.data_cnt) {
-            std::cout << now.data[l++].value << ' ' ;
-            l_string = now.data[l].index;
-            cnt++;
         }
-        num = all[num].next_num;
+        if(now.data[now.data_cnt - 1] < a) {
+            now.data[now.data_cnt] = a;
+        }else if(now.data[0] >= a) {
+            for(int i = now.data_cnt - 1;i >= 0;--i) {
+                now.data[i + 1] = now.data[i];
+            }
+            now.data[0] = a;
+        }else {
+            for(int i = now.data_cnt - 1;i >= l;--i) {
+                now.data[i + 1] = now.data[i];
+            }
+            now.data[l] = a;
+        }
+        now.data_cnt++;
+        if(now.data_cnt > data_cnt_limit) {
+            block_cnt++;
+            block<T> new_block;
+            nodeHead<T> &next_head = all[all[num].next_num];
+            nodeHead<T> &new_head = all[block_cnt - 1];
+            for(int i = 0;i < data_cnt_limit / 2;++i) {
+                std::swap(new_block.data[data_cnt_limit / 2 - 1 - i],now.data[now.data_cnt - 1 - i]);
+            }
+            new_block.data_cnt = data_cnt_limit / 2;
+            now.data_cnt -= data_cnt_limit / 2;
+            new_head.data_max = all[num].data_max;
+            all[num].data_max = now.data[now.data_cnt - 1];
+            T tmp = now.data[now.data_cnt - 1];
+            new_head.data_min = ++tmp;
+            new_head.block_pos = block_memory_river.write(new_block);
+            new_head.head_num = block_cnt - 1;
+            new_head.head_pos = sizeof(nodeHead<T>) * new_head.head_num + 8;
+            all[num].next_num = new_head.head_num;
+            new_head.next_num = next_head.head_num;
+            block_memory_river.update(now,all[num].block_pos);
+            all[new_head.head_num] = new_head;
+        }else {
+            block_memory_river.update(now,all[num].block_pos);
+        }
+    }
+    bool find(T &a) {
+        int num = 0;
+        T &Max = all[num].data_max,&Min = all[num].data_min;
+        while(num != 1) {
+            if(Max >= a && Min <= a) {
+                break;
+            }
+            num = all[num].next_num;
+            Max = all[num].data_max;
+            Min = all[num].data_min;
+        }
+        int cnt = 0;
+        block<T> now;
         block_memory_river.read(now,all[num].block_pos);
-    }
-    if(cnt == 0) {
-        std::cout << "null";
-    }
-    std::cout << '\n';
-}
-int main()
-{
-    int n;
-    std::cin >> n;
-    std::string order;
-    MemoryRiver<nodeHead> head_memory_river("head");
-    MemoryRiver<block> block_memory_river("block");
-    ifstream h("head");
-    ifstream b("block");
-    if(!h) {
-        head_memory_river.initialise("head");
-        head_memory_river.write_info(2,1);
-        nodeHead begin;
-        begin.head_pos = head_memory_river.write(begin);
-        begin.head_num = 0;
-        begin.block_pos = 8;
-        nodeHead end;
-        end.head_pos = 168;
-        end.head_num = 1;
-        head_memory_river.write(end);
-        begin.next_num = end.head_num;
-        head_memory_river.update(begin,8);
-    }
-    if(!b) {
-        block_memory_river.initialise("block");
-        block begin;
-        block_memory_river.write(begin);
-    }
-    head_memory_river.get_info(block_cnt,1);
-    head_memory_river.Read(all,block_cnt);
-    head_memory_river.get_info(block_cnt,1);
-    head_memory_river.Read(all,block_cnt);
-    for(int i = 0;i < n;++i) {
-        std::cin >> order;
-        if(order == "insert") {
-            int value;
-            string index;
-            std::cin >> index >> value;
-            insert(block_memory_river,index,value);
+        while(Max >= a && Min <= a && num != 1) {
+            if(now.data_cnt == 0) {
+                num = all[num].next_num;
+                block_memory_river.read(now,all[num].block_pos);
+                continue;
+            }
+            int l = 0,r = now.data_cnt;
+            while(l < r) {
+                int mid = (l + r) / 2;
+                if(now.data[mid] < a) {
+                    l = mid + 1;
+                }else {
+                    r = mid;
+                }
+            }
+            if(now.data[l] == a) {
+                a = now.data[l];
+                return true;
+            }
+            num = all[num].next_num;
+            block_memory_river.read(now,all[num].block_pos);
         }
-        if(order == "delete") {
-            int value;
-            string index;
-            std::cin >> index >> value;
-            Delete(block_memory_river,index,value);
+        return false;
+    }
+    void Delete(T &a){
+        int num = 0;
+        while (num != 1) {
+            if(all[num].data_max >= a && a >= all[num].data_min) {
+                break;
+            }
+            num = all[num].next_num;
         }
-        if(order == "find") {
-            string index;
-            std::cin >> index;
-            find(block_memory_river,index);
+        block<T> now;
+        block_memory_river.read(now,all[num].block_pos);
+        int l = 0,r = now.data_cnt;
+        while(l < r) {
+            int mid = (l + r) / 2;
+            if(now.data[mid] < a) {
+                l = mid + 1;
+            }else {
+                r = mid;
+            }
+        }
+        if(now.data[l] == a) {
+            now.data[l] = 0;
+            for(int i = l;i < now.data_cnt;++i) {
+                now.data[i] = now.data[i + 1];
+            }
+            now.data_cnt--;
+            block_memory_river.update(now,all[num].block_pos);
+        }else {
+            return;
         }
     }
-    head_memory_river.write_info(block_cnt,1);
-    head_memory_river.Update(all,block_cnt);
-    return 0;
-}
+private:
+    int block_cnt = 0;
+    MemoryRiver<block<T>> block_memory_river;
+    MemoryRiver<nodeHead<T>> head_memory_river;
+    nodeHead<T> all[6000]{};
+};
 
 #endif //STORE_HPP
